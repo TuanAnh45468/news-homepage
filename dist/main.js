54ba20ea4d71afa92a1b87c2e0c0e9e89725d74a
tuana <tuananh45468@gmail.com>
2023-05-17 19:54:41 +0700

    active state

--- dist/main.js
+++ dist/main.js
@@ -5,10 +5,10 @@ const navbar = document.getElementById("navbar");
 
 menu.addEventListener("click", () => {
   overlay.classList.remove("hidden");
-  navbar.classList.remove("hidden");
+  navbar.classList.remove("max-laptop:hidden");
 });
 
 close.addEventListener("click", () => {
-  navbar.classList.add("hidden");
+  navbar.classList.add("max-laptop:hidden");
   overlay.classList.add("hidden");
 });
